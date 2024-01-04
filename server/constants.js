38412c8bd89ee6951a4ba520cd053c0a2c844f00
Manish Thakur <manishthakur231690@gmail.com>
2024-01-04 07:22:58 +0530

    Config deploy

---
+++ server/constants.js
@@ -0,0 +1,6 @@
+const MongoDB =
+  "mongodb+srv://manishthakur231690:DisneyHotstar231690@cluster0.yrd3pqp.mongodb.net/?retryWrites=true&w=majority";
+
+const jwtSecretKey = "Disney@Hotstar231690Secretkey";
+
+module.exports = { MongoDB, jwtSecretKey };
